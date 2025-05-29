2a06619a2087c5492a7fd87c55f07fc204105982
Jander81 <jander81@byui.edu>
2025-05-28 17:26:24 -0700

    window resize

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -5,3 +5,16 @@ let menuLinks= document.getElementById("menu_links");
 button.addEventListener("click",function () {
     menuLinks.classList.toggle("hide"); 
 });
+
+function handleResize() {
+    let links= document.getElementById("menu_links");
+    let width= window.innerWidth;
+    if(width > 1000) {
+        links.classList.remove("hide");
+    } else {
+        links.classList.add("hide");
+    }
+}
+
+window.addEventListener("resize", handleResize);
+window.addEventListener("load", handleResize);
